49f68b00555807df5c980161cd431045e44d3dcd
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-06-24 19:50:54 +0200

    Expand basic framework

--- src/main/java/com/frederikam/hackweek2019/CommandManager.kt
+++ src/main/java/com/frederikam/hackweek2019/CommandManager.kt
@@ -2,8 +2,11 @@ package com.frederikam.hackweek2019
 
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.sharding.ShardManager
 
 class CommandManager : ListenerAdapter() {
+
+    lateinit var jda: ShardManager
 
     override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
         if (event.author.isBot) return
--- src/main/java/com/frederikam/hackweek2019/main.kt
+++ src/main/java/com/frederikam/hackweek2019/main.kt
@@ -1,13 +1,15 @@
 package com.frederikam.hackweek2019
 
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
-import net.dv8tion.jda.api.sharding.ShardManager
+
+private val commandManager = CommandManager()
 
 fun main() {
-
+    val jda = jdaBuilder().build()
+    commandManager.jda = jda
 }
 
-fun jdaBuilder(): ShardManager = DefaultShardManagerBuilder().apply {
+fun jdaBuilder() = DefaultShardManagerBuilder().apply {
     setToken(System.getenv("DISCORD_TOKEN"))
-    addEventListeners(CommandManager())
-}.build()
+    addEventListeners(commandManager)
+}
